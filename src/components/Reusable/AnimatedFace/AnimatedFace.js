747c244795b737d27d39c8c39f67dd33d94f456a
Zeitgeist J <shecksk8nick@gmail.com>
2021-01-03 22:18:13 -0500

    added shadow to animations

--- src/components/Reusable/AnimatedFace/AnimatedFace.js
+++ src/components/Reusable/AnimatedFace/AnimatedFace.js
@@ -1,10 +1,13 @@
 import './AnimatedFace.css';
 
 const AnimatedFace = ({ success }) => (
-  <div className={`animated-face-container center ${success ? 'jump-animation' : 'rotate-animation'}`}>
-    <div className={!success ? 'face-rotate-animation' : undefined}>
-      <h3>{success ? '◕‿◕' : ' ಠ_ಠ'}</h3>
+  <div className="center column face-animation-container">
+    <div className={`animated-face-container center ${success ? 'jump-animation' : 'rotate-animation'}`}>
+      <div className={!success ? 'face-rotate-animation' : undefined}>
+        <h3>{success ? '◕‿◕' : ' ಠ_ಠ'}</h3>
+      </div>
     </div>
+    <div className={`shadow ${success ? 'shadow-jump-animation' : 'rotate-animation'}`}></div>
   </div>
 )
 
